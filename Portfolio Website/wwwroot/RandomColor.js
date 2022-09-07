c95b80ad225f2866046ae72f6173143be39ddbee
Joe Simopoulos <joesimop8@gmail.com>
2022-09-07 15:44:34 -0700

    Design Overhaul

---
+++ Portfolio Website/wwwroot/RandomColor.js
@@ -0,0 +1,12 @@
+﻿var underlineColors = ['#56E39F', '#3772FF', '#DF2935'];
+
+async function ChangeBackgroundColor(element) {
+    element.style.setProperty('--underlinecolor', underlineColors[Math.floor(Math.random() * underlineColors.length)]);
+};
+
+async function UpdateView(element) {
+    console.log(element);
+    if (document.querySelector('a.link.active') !== null) {
+        document.querySelector('a.link.active').classList.remove('active');
+    }
+}
